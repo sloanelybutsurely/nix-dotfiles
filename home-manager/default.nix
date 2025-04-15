65c10866ec3bd1ca17f57381d12f4f3010f8d92f
sloane <git@sloanelybutsurely.com>
2025-04-15 13:18:08 -0400

    set EDITOR again

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -10,6 +10,7 @@
 
     sessionVariables = {
       SSH_AUTH_SOCK = "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+      EDITOR = "nvim";
     };
 
     packages = with pkgs; [
