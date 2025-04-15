92947ba399609600d4a571c5fce866ff1231f47d
sloane <git@sloanelybutsurely.com>
2025-04-15 13:16:26 -0400

    Revert "update packages, fix some settings, set $EDITOR"
    This reverts commit 0ce152bca9197d1293e05657cc7c276035045754.

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -10,7 +10,6 @@
 
     sessionVariables = {
       SSH_AUTH_SOCK = "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
-      EDITOR = "nvim";
     };
 
     packages = with pkgs; [
@@ -190,7 +189,7 @@
         email = "git@sloanelybutsurely.com";
       };
       signing = {
-        behavior = "own";
+        sign-all = true;
         backend = "ssh";
         key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID0TH2GezEx8+zlKBqUb7rBsbmghnd1u4nX6YpQr28Zw";
         backends.ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
